7ddfeb5d703e6aac1c70e2d2d24c6216f1620240
Raheem Lawal <raheem@Aaisyas-MacBook-Air.local>
2021-02-27 12:36:03 -0800

    update redirect 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Main from './Main'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
