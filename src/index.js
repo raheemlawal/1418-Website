27267d67a3c6842474aead5ac6ae3d09c36264a5
Raheem Lawal <raheem@Aaisyas-MacBook-Air.local>
2021-02-27 12:33:53 -0800

    update redirect 4

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import Main from './Main'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Main />
+    <Switch > {/* The Switch decides which component to show based on the current URL.*/}
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/portfolio' component={Portfolio}></Route>
+      <Route exact path='/contact' component={Contact}></Route>
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
