c0518624a66ace75d9e69359a875b01073aa9d31
Raheem Lawal <lawalraheem@gmail.com>
2022-10-11 20:10:25 -0700

    portfolio base

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,33 @@
 import '../styles/Portfolio.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container} from 'react-bootstrap';
+import {Container, Card, Row, Col, Image} from 'react-bootstrap';
+import thanks from '../thankspic.png';
+import landlorde from '../lanlordepic.png';
+import heem from '../heempic.png';
+import raheem from '../raheempic.png';
 
 function Portfolio() {
 
     return (
         <Container id="portfolio">
             <Container id = "main-container">
-                <h1 className = "comingsoon-text">Coming soon!</h1>
+                <h1 className='portfolio-text'>Portfolio</h1>
+                <Row id = "portrow">
+                    <Col>
+                        <Image rounded src={heem} height={300}></Image>
+                    </Col>
+                    <Col>
+                        <Image rounded src={thanks} height={300}></Image>
+                    </Col>
+                </Row>
+                <Row id = "portrow2">
+                    <Col>
+                        <Image rounded src={landlorde} height={300}></Image>
+                    </Col>
+                    <Col>
+                        <Image rounded src={raheem} height={300}></Image>
+                    </Col>
+                </Row>
             </Container>
         </Container>
     );
